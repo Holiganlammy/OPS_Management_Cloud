8c08a35da373a0fdb0aa1fbbd3f577b33cb965ec
teerapon <the_trp@hotmail.com>
2025-06-27 11:59:57 +0700

    commit

---
+++ backend-usermanage/src/main-app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PTEC_USERRIGHT_Module } from './PTEC_USERIGHT/app.module';
+import { PTEC_OPS_SMART_Module } from './PTEC_OPS_SMART/app.module';
+
+@Module({
+  imports: [PTEC_USERRIGHT_Module, PTEC_OPS_SMART_Module],
+})
+export class MainAppModule {}
